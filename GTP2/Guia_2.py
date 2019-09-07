c79a1516b9d5e8c9646a198bdab1b2cd479a41f1
Joaquin <joaquin.gonzalez6693@gmail.com>
2019-09-07 19:16:38 -0300

    casi terminadas las guias, hechos los jupyter que corren

--- GTP2/Guia_2.py
+++ GTP2/Guia_2.py
@@ -71,22 +71,24 @@ for i in range(len(info['chs'])):
 	
 datos = data['Datos']
 datos = np.swapaxes(datos,0,2)
+datos_eeg = datos[:,:-3,:]
 tiempo = [i/sampling_freq for i in range(len(datos[0][0]))]
 
 #APLICAR PCA
 sk_pca = PCA()
 pca = UnsupervisedSpatialFilter(sk_pca, average = False)
-data_pca = pca.fit_transform(datos)
+data_pca = pca.fit_transform(datos_eeg)
 
 #PLOT PRINCIPAL COMPONENTS
 W = sk_pca.components_
 M = np.linalg.inv(W)
+M = M.transpose()
 
 fig = plt.figure(figsize=(15, 25))
 for i in range(len(M)):
 	ax1 = fig.add_subplot(3,9,(i+1))
 	ax1.set_title('PCA {}'.format(i))
-	mne.viz.plot_topomap(M[i][:-3], np.array(channel_pos)[:-3], axes = ax1)
+	mne.viz.plot_topomap(M[i], np.array(channel_pos)[:-3], axes = ax1)
 fig.tight_layout()
 fig.suptitle('Principal components')
 
@@ -100,36 +102,39 @@ for i in range(len(datos[0])):
 	ax1.grid()
 	ax1.set_ylabel('{}'.format(channel_names[i]))
 	ax1.plot(tiempo, datos[0][i])
-	ax2 = fig2.add_subplot(13,2,i+1)
-	ax2.grid()
-	ax2.set_ylabel('{}'.format(channel_names[i]))
-	ax2.plot(tiempo, data_pca[0][i])
 	ax1.set_xlim([0,2])
-	ax2.set_xlim([0,2])
 	ax1.tick_params(axis='both', which='major', labelsize=8)
-	ax2.tick_params(axis='both', which='major', labelsize=8)
 	if i < len(datos[0])-2:
 		ax1.set_xticklabels([])
-		ax2.set_xticklabels([])
 	else:
 		ax1.set_xlabel('time[s]')
-		ax2.set_xlabel('time[s]')
 fig1.suptitle('Raw data')
+
+for i in range(len(data_pca[0])):	
+	ax2 = fig2.add_subplot(13,2,i+1)
+	ax2.grid()
+	ax2.set_ylabel('PCA {}'.format(i))
+	ax2.plot(tiempo, data_pca[0][i])
+
+	ax2.set_xlim([0,2])
+	ax2.tick_params(axis='both', which='major', labelsize=8)
+	if i < len(data_pca[0])-2:
+		ax2.set_xticklabels([])
+	else:
+		ax2.set_xlabel('time[s]')
 fig2.suptitle('Principal components')
 
-	
 #%% ICA
+from mne.preprocessing import ICA
 from mne.decoding import UnsupervisedSpatialFilter
 from sklearn.decomposition import FastICA
 
 data = spio.loadmat('Datos/DataEEG_EOG_bis.mat')
-
 sampling_freq = data['sf'][0][0]
-
 datos = data['Datos']
 datos = np.swapaxes(datos,0,2)
+datos_eeg = datos[:,:-3,:]
 tiempo = [i/sampling_freq for i in range(len(datos[0][0]))]
-
 channel_names = [data['ChannelNames'][i][0][0]for i in range(len(data['ChannelNames']))]
 tipos = ['eeg' for i in range(len(data['ChannelNames'])-3)]+['eog' for i in range(3)]
 info = mne.create_info(ch_names = channel_names, sfreq = sampling_freq, ch_types = tipos, montage = 'standard_1020')
@@ -137,76 +142,82 @@ channel_pos = []
 for i in range(len(info['chs'])):
 	channel_pos.append(info['chs'][i]['loc'][:2])
 	
-eeg = mne.EpochsArray(datos, info)
-eeg.set_montage('standard_1020')
-eeg_temp = eeg.copy()
+# MNE 
+#eeg = mne.EpochsArray(datos, info)
+#eeg.set_montage('standard_1020')
+#eeg_temp = eeg.copy()
+#
+#ica = ICA(method = 'fastica', random_state = 1)
+#ica.fit(eeg_temp)
 
-ica = mne.preprocessing.ICA(method = 'extended-infomax', random_state = 1)
-ica.fit(eeg_temp)
+#PLOT INDEPENDENT COMPONENTS MAL PUES ICA HACE PCA ANTES, ENTONCES LA MATRIZ VUELVE AL ESPACIO
+#PCA EN VEZ DEL RAW
 
-#ica.plot_components(inst = eeg_temp) 
-#Esto grafica los pesos de cada electrodo en cada componente independiente (W_trans)
-# yo queria los componentes independiente (inv W)
+#UnMix = ica.unmixing_matrix_
+#Mix = ica.mixing_matrix_
+#
+#fig = plt.figure(figsize=(15, 25))
+#for i in range(len(Mix)):
+#	ax1 = fig.add_subplot(3,9,(i+1))
+#	ax1.set_title('ICA {}'.format(i))
+#	mne.viz.plot_topomap(UnMix[i], np.array(channel_pos)[:-3], axes = ax1)
+#fig.tight_layout()
+#fig.suptitle('Independent components mne')
 
+# PLOT ICA COMPONENTS BIEN
+#ica.plot_components()
 
-#PLOT INDEPENDENT COMPONENTS
-W = ica.get_components()
-M = np.linalg.inv(W)
-
-fig = plt.figure(figsize=(15, 25))
-for i in range(len(M)):
-	ax1 = fig.add_subplot(3,9,(i+1))
-	ax1.set_title('ICA {}'.format(i))
-	mne.viz.plot_topomap(M[i], np.array(channel_pos)[:-3], axes = ax1)
-fig.tight_layout()
-fig.suptitle('Independent components mne')
-
-# MEDIANTE SKLEARN FASTICA LOS PATRONES DAN DISTINTO
+# MEDIANTE SKLEARN FASTICA 
 sk_ica = FastICA()
 ICA_transformer = UnsupervisedSpatialFilter(sk_ica, average = False)
-data_ICA = ICA_transformer.fit_transform(datos)
-
-W = sk_ica.components_
-M = np.linalg.inv(W)
+data_ICA = ICA_transformer.fit_transform(datos_eeg)
+Mix = sk_ica.mixing_.transpose()
 
 fig = plt.figure(figsize=(15, 25))
-for i in range(len(M)):
+for i in range(len(Mix)):
 	ax1 = fig.add_subplot(3,9,(i+1))
 	ax1.set_title('ICA {}'.format(i))
-	mne.viz.plot_topomap(M[i][:-3], np.array(channel_pos)[:-3], axes = ax1)
+	mne.viz.plot_topomap(Mix[i], np.array(channel_pos)[:-3], axes = ax1)
 fig.tight_layout()
 fig.suptitle('Independent components sklearn')
 
-###-------- PLOT RAW DATA AND INDEPENDENT COMPONENTS -------###
-fig1 = plt.figure(figsize=(10,20))
-plt.subplots_adjust(left = 0.07, bottom=0.08, right=0.95, top=0.93, wspace = 0.17, hspace = 0.3)
+###-------- PLOT AND INDEPENDENT COMPONENTS -------###
 fig2 = plt.figure(figsize=(10,20))
-plt.subplots_adjust(left = 0.1, bottom=0.08, right=0.95, top=0.93, wspace = 0.25, hspace = 0.3)
-for i in range(len(datos[0])):
-	ax1 = fig1.add_subplot(13,2,(i+1))
-	ax1.grid()
-	ax1.set_ylabel('{}'.format(channel_names[i]))
-	ax1.plot(tiempo, datos[0][i])
-	ax2 = fig2.add_subplot(13,2,i+1)
-	ax2.grid()
-	ax2.set_ylabel('{}'.format(channel_names[i]))
-	ax2.plot(tiempo, data_ICA[0][i])
-	ax1.set_xlim([0,2])
-	ax2.set_xlim([0,2])
-	ax1.tick_params(axis='both', which='major', labelsize=8)
-	ax2.tick_params(axis='both', which='major', labelsize=8)
-	if i < len(datos[0])-2:
-		ax1.set_xticklabels([])
-		ax2.set_xticklabels([])
-	else:
-		ax1.set_xlabel('time[s]')
-		ax2.set_xlabel('time[s]')
-fig1.suptitle('Raw data')
+plt.subplots_adjust(left = 0.1, bottom=0.1, right=0.95, top=0.93, wspace = 0.25, hspace = 0.3)
+for i in range(len(data_ICA[0])):
+    ax2 = fig2.add_subplot(11,2,i+1)
+    ax2.grid()
+    ax2.set_ylabel('ICA{}'.format(i))
+    ax2.plot(tiempo, data_ICA[150][i])
+    ax2.set_xlim([0,2])
+    ax2.tick_params(axis='both', which='major', labelsize=8)
+    if (i < len(data_ICA[0])-2):
+        ax2.set_xticklabels([])
+    else:
+        ax2.set_xlabel('time[s]')
 fig2.suptitle('Independent components')
 
-ica.exclude = [8, 9, 14]
+#EXCLUDE ARTIFACTS AND PLOT FILTERED AND RAW DATA 
+exclude_channels = [1,3,5,15]
+data_ICA[:, exclude_channels, :] = 0
+reconstructed_data = ICA_transformer.inverse_transform(data_ICA)
 
-# AHORA HABRIA QEU VOLVER A ARMAR LA SEÑAL ORIGINAL SEPARADA POR ELECTRODOS
+fig1 = plt.figure(figsize=(10,20))
+plt.subplots_adjust(left = 0.07, bottom=0.08, right=0.95, top=0.93, wspace = 0.17, hspace = 0.3)
+for i in range(len(reconstructed_data[0])):
+	ax1 = fig1.add_subplot(11,2,(i+1))
+	ax1.grid()
+	ax1.set_ylabel('{}'.format(channel_names[i]))
+	ax1.plot(tiempo, reconstructed_data[0][i], label= 'filtered data')
+	ax1.plot(tiempo, datos[0][i], label = 'raw data')
+	ax1.set_xlim([0,2])
+	ax1.tick_params(axis='both', which='major', labelsize=8)
+	if (i < len(reconstructed_data)-2):
+		ax1.set_xticklabels([])
+	else:
+		ax1.set_xlabel('time[s]')
+plt.legend(bbox_to_anchor=(1, 0.5))
+fig1.suptitle('Filtered data')
 
 #%% CSP
 from mne.decoding import CSP
@@ -217,7 +228,7 @@ sampling_freq = data['sf'][0][0]
 datos = data['Datos']
 datos = np.swapaxes(datos,0,2)
 tiempo = [i/sampling_freq for i in range(len(datos[0][0]))]
-datos_filt = abs(butter_bandpass_filter(datos, [8, 30], sampling_freq, 5, axis = 2))
+datos_filt = butter_bandpass_filter(datos, [8, 30], sampling_freq, 2, axis = 2)
 
 labels = data['etiquetas'][0]
 channel_names = [data['ChannelNames'][i][0][0]for i in range(len(data['ChannelNames']))]
@@ -251,10 +262,8 @@ for j,canal in enumerate(canales):
 	k += 1
 	if j < len(canales)-2:
 		ax1.set_xticklabels([])
-		ax2.set_xticklabels([])
 	else:
 		ax1.set_xlabel('time[s]')
-		ax2.set_xlabel('time[s]')
 	
 #GRAFICO LA MISMA CLASE EN LOS 3 PARES DE CANALES PARA VER CAMBIO DE VARIANZA
 for i in [0,1]:
@@ -271,8 +280,7 @@ for i in [0,1]:
 		ax1.legend()
 		k+=1
 	
-
-#NO SE VE MUCHO EN LOS GRAFICOS ENTONCES VEO EL PROMEDIO DE LA VARIANZA DE CLADA CLASE PARA CADA CANAL
+#VEO EL PROMEDIO DE LA VARIANZA DE CADA CLASE PARA CADA CANAL
 indexes_1 = get_indexes(labels, 1)
 indexes_2 = get_indexes(labels, 2)
 
@@ -290,7 +298,20 @@ for i in range(len(eeg_CSP[0])):
 	varianza_final_1.append(np.mean(var_1))
 	varianza_final_2.append(np.mean(var_2))
 
-	
+dif_var = [varianza_final_1[i]-varianza_final_2[i] for i in range(len(varianza_final_1))]
+
+fig = plt.figure()
+ax = fig.add_subplot(2,1,1)
+ax.plot(dif_var)
+ax.grid()
+ax.set_ylabel('Var clase 1 - var clase 2')
+ax1 = fig.add_subplot(2,1,2)	
+ax1.plot(abs(np.array(dif_var)))
+ax1.grid()
+ax1.set_xlabel('Canal')
+ax1.set_ylabel('Valor absoluto')
+fig.suptitle('Varianzas por canal')
+
 #%% 3.4
 from mne.decoding import CSP
 
@@ -331,6 +352,8 @@ for i in range(len(eeg_CSP)):
 	features_final.append(np.concatenate((features[i], features[i+288], features[i+2*288])))
 
 ###----ARMO HISTOGRAMAS PARA HACER KULLBACK ----###
+indexes_1 = get_indexes(labels, 1)
+indexes_2 = get_indexes(labels, 2)
 
 features_final = np.transpose(np.array(features_final))
 
@@ -357,12 +380,14 @@ Kullback.sort(key = operator.itemgetter(1), reverse = True)
 plt.figure()
 plt.grid()
 plt.title('Kullback-Leibler')
-plt.plot(Kullback, '.')
+plt.plot([Kullback[i][1]for i in range(len(Kullback))], '.')
 
+# CONSERVO LAS FEATURES QUE ME DAN UNA DIFERENCIA ENTER CLASES MAYOR A 0.1 SEGUN KULLBACK
 features_discriminantes = []
-for i in range(len(Kullback)):
-	indices = np.where(Kullback[i][1] > 0.1)
-	features_discriminativas.append(Kullback[i][0] np.where(Kullback[i][1] > 0.1)
+i=0
+while(Kullback[i][1]>0.1):
+	features_discriminantes.append(Kullback[i][0])
+	i+=1
 
 #%% 3.5
 import mne
@@ -399,9 +424,16 @@ for i in range(len(info['chs'])):
 	channel_pos.append(info['chs'][i]['loc'][:2])
 
 ###---- PLOT EEG RAW DATA OVER 1 TRIAL ----###
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(1,2,1)
-ax2 = fig1.add_subplot(1,2,2)
+	
+#DEFINIR PCA
+pca = PCA()
+
+fig1 = plt.figure(figsize = (15,10))
+ax1 = fig1.add_subplot(2,3,1)
+ax2 = fig1.add_subplot(2,3,2)
+ax3 = fig1.add_subplot(2,3,3)
+ax4 = fig1.add_subplot(2,3,4)
+ax5 = fig1.add_subplot(2,3,5)
 
 for i in range(200):
 	ax1.clear()
@@ -410,6 +442,15 @@ for i in range(200):
 	ax2.clear()
 	ax2.set_title('Trial 1 - RAW EEG data')
 	mne.viz.plot_topomap(trial_1[i][:-3], np.array(channel_pos[:-3]), axes = ax2)
+	
+	#APLICAR PCA
+	data_pca = pca.fit_transform(trial_1[i])
+	W = pca.components_
+	M = np.linalg.inv(W)
+	M = M.transpose()
+	
+	ax3.clear()
+	mne.viz.plot_topomap(M[i][:-3], np.array(channel_pos)[:-3], axes = ax3)
 	plt.pause(0.05)
 	
 	
--- GTP3/Guia_3.py
+++ GTP3/Guia_3.py
@@ -5,13 +5,19 @@ Created on Sat Aug 24 21:22:24 2019
 
 @author: joaco
 """
-
 import os
 import numpy as np
 import scipy.io as spio
 import matplotlib.pyplot as plt
 from scipy import signal
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC, SVC
+from sklearn.metrics import confusion_matrix, f1_score, precision_recall_curve, roc_curve, auc
+from sklearn.model_selection import cross_val_predict, cross_validate
+from sklearn.model_selection import KFold
+from mne.decoding import CSP
+import random
 
 wd = '/home/joaco/Desktop/joaco/facultad/BCI/Guías/GTP3'
 os.chdir(wd)
@@ -42,9 +48,32 @@ def butter_bandpass_filter(data, frecuencias, sampling_freq, order, axis):
 	y = signal.filtfilt(b, a, data, axis = axis, padlen = None)
 	return y
 
-#%% 3.1
-from sklearn.model_selection import train_test_split
+def butter_bandpass_live(data, frecuencias, sampling_freq, order, axis):
+	frecuencias = [frecuencias[i]/(sampling_freq/2) for i in range(len(frecuencias))]
+	b, a = signal.butter(order, frecuencias, btype='band')
+	y = signal.lfilter(b, a, data, axis = axis)
+	return y
+
+def metricas(confusion_matrix):
 	
+	taza_aciertos = (confusion_matrix[0,0] + confusion_matrix[1,1])/np.sum(confusion_matrix)
+	presicion = confusion_matrix[0,0]/(confusion_matrix[0,0]+confusion_matrix[1,0])
+	sensibilidad = confusion_matrix[0,0]/(confusion_matrix[0,0]+confusion_matrix[0,1])
+	especificidad = confusion_matrix[1,1]/(confusion_matrix[1,1]+confusion_matrix[1,0])
+	
+	return taza_aciertos, presicion, sensibilidad, especificidad
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{:.2f}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+#%% 3.1
 data_256 = spio.loadmat('Datos/Datos_Sujeto1_256.mat')
 datos_256 = np.swapaxes(data_256['Datos'], 1, 2)
 data_32 = spio.loadmat('Datos/Datos_Sujeto1_32.mat')
@@ -95,7 +124,7 @@ indexes_0 = get_indexes(labels_val, 0)
 indexes_1 = get_indexes(labels_val, 1)
 
 ###---- LDA ----###
-lda_32 = LinearDiscriminantAnalysis()
+lda_32 = LinearDiscriminantAnalysis(solver = 'eigen')
 lda_32.fit(datos_32_train, labels_train)
 
 fig = plt.figure()
@@ -115,14 +144,16 @@ ax2.plot(lda_32.transform(datos_32_val)[indexes_1], '.')
 fig.tight_layout()
 
 #%% 3.1 2
-import random
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_validate
-
 data_32 = spio.loadmat('Datos/Datos_Sujeto1_32.mat')
 datos_32 = np.swapaxes(data_32['Datos'], 1, 2)
-labels = data_256['Etiquetas']
-datos_32 = np.reshape(datos_32, [3780, 320])
+sf = 32
+tiempo = np.array([i/sf for i in range(len(datos_32[0][0]))])
+indice_250 = np.where(tiempo >= 0.25)[0][0]
+indice_650 = np.where(tiempo >= 0.65)[0][0]
+datos_32 = datos_32[:, :, indice_250:indice_650]
+
+labels = data_32['Etiquetas']
+datos_32 = np.reshape(datos_32, [3780, len(datos_32[0])*len(datos_32[0][0])])
 
 datos_32_train, datos_32_val, labels_train, labels_val = train_test_split(datos_32, labels, test_size=0.1, random_state=42)
 labels_train = np.ravel(labels_train)
@@ -130,8 +161,8 @@ labels_val = np.ravel(labels_val)
 
 indices_0 = get_indexes(labels_train, 0)
 indices_1 = get_indexes(labels_train,1)
-indices_0_630 = random.sample(indices_0, 630)
-indices_bal = indices_0_630 + indices_1
+indices_0_cant_bal = random.sample(indices_0, len(indices_1))
+indices_bal = indices_0_cant_bal + indices_1
 datos_32_bal = datos_32[indices_bal]
 labels_bal = labels_train[indices_bal]
 
@@ -156,29 +187,27 @@ plt.figure()
 plt.grid()
 plt.xlabel('Shrinkage value')
 plt.ylabel('Scores')
-plt.plot(shrink_list, means_train, '.', label='Train')
+plt.plot(shrink_list, means_train, label='Train')
 plt.errorbar(shrink_list, means_train, yerr = variances_train, fmt = 'none', ecolor = 'k', elinewidth = 1)
-plt.plot(shrink_list, means_test, '.', label='Test')
+plt.plot(shrink_list, means_test, label='Test')
 plt.errorbar(shrink_list, means_test, yerr = variances_test, fmt = 'none', ecolor = 'k', elinewidth = 1)
-plt.plot(shrink_list, means_val, '.', label='Val')
+plt.plot(shrink_list, means_val, label='Val')
 plt.legend()
 
 #%% 1.3 3
-import random
-	
 data_32 = spio.loadmat('Datos/Datos_Sujeto1_32.mat')
 datos_32 = np.swapaxes(data_32['Datos'], 1, 2)
-labels = data_256['Etiquetas']
+labels = data_32['Etiquetas']
 datos_32 = np.reshape(datos_32, [3780, 320])
 
 indices_0 = get_indexes(labels, 0)
 indices_1 = get_indexes(labels,1)
-indices_0_630 = random.sample(indices_0, 630)
+indices_0_630 = random.sample(indices_0, len(indices_1))
 indices_bal = indices_0_630 + indices_1
 datos_32_bal = datos_32[indices_bal]
 labels_bal = labels[indices_bal]
 
-tamaños = [180, 360, 720]
+tamaños = [180, 360, 720, len(datos_32_bal)]
 desempeños_trad = {}
 desempeños_reg = {}
 for tamaño in tamaños:
@@ -187,11 +216,11 @@ for tamaño in tamaños:
 	for i in range(10):
 		indices = random.sample(list(np.arange(len(datos_32_bal))), tamaño)
 		datos_train = datos_32_bal[indices]
-		labels_train = labels_bal[indices]
+		labels_train = np.ravel(labels_bal[indices])
 		datos_val = np.delete(datos_32_bal, indices, axis = 0)
 		labels_val = np.delete(labels_bal, indices, axis = 0)
 		
-		lda_trad = LinearDiscriminantAnalysis(solver='eigen')
+		lda_trad = LinearDiscriminantAnalysis()
 		lda_reg = LinearDiscriminantAnalysis(solver = 'eigen', shrinkage = 'auto')
 		lda_trad.fit(datos_train, labels_train)
 		lda_reg.fit(datos_train, labels_train)
@@ -200,9 +229,186 @@ for tamaño in tamaños:
 		
 	desempeños_trad[tamaño]=np.mean(resultados_trad)
 	desempeños_reg[tamaño]=np.mean(resultados_reg)
-		
-		
 	
+###---- PLOT SCORES ----###
+graf_labels = ['180', '360', '720', 'all']
+x = np.arange(len(graf_labels))  # the label locations
+width = 0.25 # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, desempeños_trad.values(), width, label='trad')
+rects2 = ax.bar(x + width/2, desempeños_reg.values(), width, label='reg')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores value')
+ax.set_title('Scores for traditional and regularized LDA')
+ax.set_xticks(x)
+ax.set_xticklabels(graf_labels)
+ax.legend()
+
+autolabel(rects1)
+autolabel(rects2)
+fig.tight_layout()
+plt.show()
+		
+#%% 3.2 
+
+data = spio.loadmat('Datos/DataEEG_MI.mat')
+datos = np.swapaxes(data['Data'], 0, 2)
+labels = np.ravel(data['labels'][0])
+sf = data['sf'][0][0]
+tiempo = [i/sf for i in range(len(datos[0][0]))]
+datos_filt = abs(butter_bandpass_live(datos, [8,30], sf, 5, axis = 2))
+
+channel_names = [data['Channels'][i][0][0]for i in range(len(data['Channels']))]
+tipos = ['eeg' for i in range(len(channel_names))]
+info = mne.create_info(ch_names = channel_names, sfreq = sf, ch_types = tipos)
+eeg = mne.EpochsArray(datos_filt, info)
+eeg.set_montage('standard_1020')
+eeg_temp = eeg.get_data()
+
+datos_train, datos_val, labels_train, labels_val = train_test_split(eeg_temp, labels, test_size=0.3, random_state=42)
+
+csp = CSP(n_components=22, reg=0.5, log=True, transform_into='average_power')
+eeg_CSP_train = csp.fit_transform(datos_train, labels_train)
+eeg_CSP_val = csp.fit_transform(datos_val, labels_val)
+
+features_train = [np.concatenate((eeg_CSP_train[i,:3], eeg_CSP_train[i,-3:])) for i in range(len(eeg_CSP_train))]
+features_val = [np.concatenate((eeg_CSP_val[i,:3], eeg_CSP_val[i,-3:])) for i in range(len(eeg_CSP_val))]
+
+eeg_features_train = np.array(features_train)
+eeg_features_val = np.array(features_val)
+
+cv = KFold(n_splits = 10, shuffle = True, random_state = 654781)
+
+linear = SVC(kernel = 'linear')
+scores_lineal = cross_val_predict(linear, eeg_features_train, labels_train, cv=cv)
+linear.fit(eeg_features_train, labels_train)
+linear_predictions = linear.predict(features_val)
+CM_lineal = confusion_matrix(labels_val, linear_predictions)
+linear_results = metricas(CM_lineal)
 
 
+polinomial = SVC(kernel = 'poly')
+scores_poly = cross_validate(polinomial, eeg_features_train, labels_train, cv=cv)
+polinomial.fit(eeg_features_train, labels_train)
+predictions_poly = polinomial.predict(features_val)
+CM_poly = confusion_matrix(labels_val, predictions_poly)
+poly_results = metricas(CM_poly)
 
+
+gaussian = SVC(kernel = 'rbf')
+scores_gaus = cross_validate(gaussian, eeg_features_train, labels_train, cv=cv)
+gaussian.fit(eeg_features_train, labels_train)
+predictions_gaus = gaussian.predict(features_val)
+CM_gaus = confusion_matrix(labels_val, predictions_gaus)
+gauss_results = metricas(CM_gaus)
+
+
+###---- PLOT SCORES ----###
+graf_labels = ['aciertos', 'precision', 'sensibilidad', 'especificidad']
+x = np.arange(len(graf_labels))  # the label locations
+width = 0.25 # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width, linear_results, width, label='lineal')
+rects2 = ax.bar(x, poly_results, width, label='plinomial')
+rects3 = ax.bar(x + width, gauss_results, width, label='gaussian')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores value')
+ax.set_title('Scores for linear, polinomial and agaussian kernels')
+ax.set_xticks(x)
+ax.set_xticklabels(graf_labels)
+ax.legend()
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+fig.tight_layout()
+plt.show()
+
+#%% 3.3 
+from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix, f1_score, precision_recall_curve, roc_curve, auc
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import KFold
+
+data_32 = spio.loadmat('Datos/Datos_Sujeto1_32.mat')
+datos_32 = np.swapaxes(data_32['Datos'], 1, 2)
+labels = np.ravel(data_32['Etiquetas'])
+datos_32 = np.reshape(datos_32, [3780, 320])
+
+cv = KFold(n_splits = 10, shuffle = True, random_state = 654781)
+
+###---- LDA ----###
+lda = LinearDiscriminantAnalysis(solver = 'eigen', shrinkage = 'auto')
+predictions_lda = cross_val_predict(lda, datos_32, labels, cv=cv)
+
+###---- SMV ----###
+SVM_linear = LinearSVC()
+predictions_SVM = cross_val_predict(SVM_linear, datos_32, labels, cv=cv)
+
+###---- Calculo ROC y AUC-ROC ----###
+fpr_lda, tpr_lda, tresholds = roc_curve(labels, predictions_lda)
+fpr_SVM, tpr_SVM, tresholds = roc_curve(labels, predictions_SVM)
+
+roc_auc_SVM = auc(fpr_SVM, tpr_SVM)
+roc_auc_lda = auc(fpr_lda, tpr_lda)
+
+###---- Caluclo PR y PR-AUC ----###
+pr_lda, re_lda, thresholds = precision_recall_curve(labels, predictions_lda)
+pr_SVM, re_SVM, thresholds = precision_recall_curve(labels, predictions_SVM)
+
+PR_auc_lda = auc(re_lda, pr_lda)
+PR_auc_SVM = auc(re_SVM, pr_lda)
+
+###---- Caluclo F1 score ----###
+lda_f1 = f1_score(labels, predictions_lda)
+SVM_f1 = f1_score(labels, predictions_SVM)
+
+fig = plt.figure(figsize = (15, 10))
+
+ax1 = plt.subplot(121)
+ax1.plot(fpr_lda, tpr_lda, lw=1, label='LDA (auc = %0.3f)' % roc_auc_lda)
+ax1.plot(fpr_SVM, tpr_SVM, color='darkorange', lw=1, label='SVM (auc = %0.3f)' % roc_auc_SVM)
+ax1.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
+ax1.set_xlim([0.0, 1.0])
+ax1.set_ylim([0.0, 1.05])
+ax1.set_xlabel('False Positive Rate')
+ax1.set_ylabel('True Positive Rate')
+ax1.set_title('ROC curve')
+ax1.grid()
+ax1.legend(loc="lower right")
+
+ax2 = plt.subplot(122)
+ax2.plot(re_lda, pr_lda, lw=1, label='LDA (auc = %0.3f)' % PR_auc_lda)
+ax2.plot(re_SVM, pr_SVM, color='darkorange', lw=1, label='SVM (auc = %0.3f)' % PR_auc_SVM)
+ax2.set_xlim([0.0, 1.0])
+ax2.set_ylim([0.0, 1.05])
+ax2.set_xlabel('Presicion')
+ax2.set_ylabel('Recall')
+ax2.set_title('2-class Precision-Recallcurve')
+ax2.legend(loc="lower left")
+ax2.grid()
+
+###---- PLOT RESULTS ----###
+graf_labels = ['ROC-AUC', 'PR-AUC', 'F1']
+x = np.arange(len(graf_labels))  # the label locations
+width = 0.25 # the width of the bars
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, [roc_auc_lda, PR_auc_lda, lda_f1], width, label='LDA')
+rects2 = ax.bar(x + width/2, [roc_auc_SVM, PR_auc_SVM, SVM_f1], width, label='SVM')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Scores value')
+ax.set_title('Scores for LDA and SVM')
+ax.set_xticks(x)
+ax.set_xticklabels(graf_labels)
+ax.legend()
+
+autolabel(rects1)
+autolabel(rects2)
+fig.tight_layout()
+plt.show()
